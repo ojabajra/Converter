0bc928d0aeb2e46ae5dcda49402884ca482b1736
Oja Bajracharya <oja.bajracharya@gmail.com>
2026-06-10 17:34:43 +0200

    add forcing deployment

--- build-and-push.sh
+++ build-and-push.sh
@@ -16,3 +16,16 @@ aws ecr get-login-password --region "$AWS_REGION" \
 docker build --platform linux/amd64 -t plat-converter . -f ./env/Dockerfile
 docker tag plat-converter:latest $ECR_URL:latest
 docker push $ECR_URL:latest
+
+read -r -p "Force a new ECS deployment? [y/N] " REPLY
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  cd "$PROJECT_ROOT/terraform"
+  aws ecs update-service \
+    --no-cli-pager \
+    --cluster $(terraform output -raw ecs_cluster_name) \
+    --service  $(terraform output -raw ecs_service_name) \
+    --force-new-deployment
+  echo "Deployment triggered."
+else
+  echo "Skipping deployment."
+fi
